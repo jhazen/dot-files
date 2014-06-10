d2ca177909e808bc74523b11e208c413d29ac8fa
Jesse Hazen <jhazen532@gmail.com>
2014-06-10 12:20:38 -0700

    Completed install.sh support for remaining files

--- install.sh
+++ install.sh
@@ -25,10 +25,10 @@ fi
 function vimsetup() {
   echo "vim"
   if [ -f ~/.vimrc ]; then
-    mv ~/.vimrc $BACKUP_DIR/vimrc
+    mv ~/.vimrc $BACKUP_DIR/vimrc-$(date +%s)
   fi
   if [ -d ~/.vim ]; then
-    mv ~/.vim $BACKUP_DIR/vim
+    mv ~/.vim $BACKUP_DIR/vim-$(date +%s)
   fi
   git clone https://github.com/gmarik/vundle.vim ~/.vim/bundle/vundle
   ln -s $DOTFILES/vimrc ~/.vimrc
@@ -44,10 +44,10 @@ function zshsetup() {
     rm ~/.zshenv
   fi
   if [ -f ~/.zshrc ]; then
-    mv ~/.zshrc $BACKUP_DIR/zshrc
+    mv ~/.zshrc $BACKUP_DIR/zshrc-$(date +%s)
   fi
   if [ -f ~/.zshenv ]; then
-    mv ~/.zshenv $BACKUPDIR/zshenv
+    mv ~/.zshenv $BACKUPDIR/zshenv-$(date +%s)
   fi
   ln -s $DOTFILES/zshrc ~/.zshrc
   ln -s $DOTFILES/zshenv ~/.zshenv
@@ -59,24 +59,57 @@ function openboxsetup() {
 
 function conkysetup() {
   echo "conky"
+  if [ -L ~/.conkyrc ]; then
+    rm ~/.conkyrc
+  fi
+  if [ -f ~/.conkyrc ]; then
+    mv ~/.conkyrc $BACKUP_DIR/conkyrc-$(date +%s)
+  fi
+  ln -s $DOTFILES/conkyrc ~/.conkyrc
 }
 
 function wbarsetup() {
   echo "wbar"
+  if [ -L ~/.wbar ]; then
+    rm ~/.wbar
+  fi
+  if [ -f ~/.wbar ]; then
+    mv ~/.wbar $BACKUP_DIR/wbar-$(date +%s)
+  fi
+  ln -s $DOTFILES/wbar ~/.wbar
 }
 
 function tint2setup() {
   echo "tint2"
+  if [ -L ~/.config/tint2/tint2rc ]; then
+    rm ~/.config/tint2/tint2rc
+  fi
+  if [ -f ~/.config/tint2/tint2rc ]; then
+    mv ~/.config/tint2/tint2rc $BACKUP_DIR/tint2rc-$(date +%s)
+  fi
+  ln -s $DOTFILES/tint2rc ~/.config/tint2/tint2rc
 }
 
 function gtk2setup() {
   echo "gtk2"
+  if [ -L ~/.gtkrc-2.0 ]; then
+    rm ~/.gtkrc-2.0
+  fi
+  if [ -f ~/.gtkrc-2.0 ]; then
+    mv ~/.gtkrc-2.0 $BACKUP_DIR/gtkrc-2.0-$(date +%s)
+  fi
+  ln -s $DOTFILES/gtkrc-2.0 ~/.gtkrc-2.0
 }
 
 function allsetup() {
   echo "all"
   vimsetup
   zshsetup
+  openboxsetup
+  conkysetup
+  wbarsetup
+  tint2setup
+  gtk2setup
 }
 
 ### Verify and run input selections
