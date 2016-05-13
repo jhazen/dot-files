c974d5ce20edd2fc70a851f50a73be62d24d4780
Jesse Hazen <jhazen532@gmail.com>
2016-05-13 14:36:59 -0700

    Added specs() fab call

--- fabfile.py
+++ fabfile.py
@@ -11,6 +11,17 @@ def df():
 def whoami():
     fab.run('whoami')
 
+def uptime():
+    fab.run('uptime')
+
+def specs():
+    numprocraw = fab.run('cat /proc/cpuinfo | grep processor | tail -n 1 | cut -d":" -f2')
+    numproc = int(numprocraw) + 1
+    nummemraw = fab.run('cat /proc/meminfo  | head -n 1 | cut -d":" -f2')
+    nummem = int(nummemraw.split(" ")[0]) / 1024000 + 1
+    print "Num processors:", numproc
+    print "Total RAM:", str(nummem) + "G"
+
 def get_file(rfile):
     lfile = rfile.split("/")[-1]
     hname = fab.run("hostname")
