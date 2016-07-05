0f637d3c5c3808f436ad85a7f7cf2fb8d9af6b25
Jesse Hazen <jhazen532@gmail.com>
2016-07-05 09:12:41 -0700

    Added psycopg2

--- pythonrc.py
+++ pythonrc.py
@@ -12,6 +12,7 @@ import fabric.api as fab
 import bs4 as bs
 from sh import ls, pwd, cd, git, wc, grep, sed, awk, tar, zip, unzip, cat, sort, mv, chown, chmod
 import sqlite3
+import psycopg2
 
 #PS1 variable
 sys.ps1 = "$ "
