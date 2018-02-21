4dd8fcf41c69573df5dcf6f3ff6cdc9c43b20ddf
Jesse Hazen <jhazen532@gmail.com>
2018-02-20 20:58:48 -0800

    Added portforwarding in vimserver.py

--- vimserver.py
+++ vimserver.py
@@ -10,6 +10,13 @@ names = []
 ips = []
 list_location = os.environ.get("VAGRANTLAB")
 
+fp_map = {
+    "wildfly11": 8080,
+    "web": 8080,
+    "ci": 8080,
+    "db": 5432
+}
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--chef", help="set provisioner to chef (salt is default)", action="store_true")
 parser.add_argument("role", type=str, help="Name of saltstack role to apply. i.e. appserver")
@@ -19,6 +26,11 @@ if args.chef:
     provisioner = "chef"
 else:
     provisioner = "salt"
+
+if args.role in fp_map:
+    fp = True
+else:
+    fp = False
 
 with open("{}/servers.yaml".format(list_location), 'r') as file:
     servers = yaml.load(file)
@@ -39,7 +51,15 @@ for val in range(102, 210):
     if addr in ips:
         continue
     else:
+        hport = val + 8000
         break
+
+if fp:
+    fp_yaml =  "forwarded_port:\n"
+    fp_yaml += "    - guest: {}\n".format(fp_map[args.role])
+    fp_yaml += "      host: {}\n  ".format(hport)
+else:
+    fp_yaml = ""
 
 final_yaml = """
 - name: {}
@@ -47,10 +67,12 @@ final_yaml = """
   ip: {}
   provision: {}
   temp: true
-  role: {}""".format(tag, addr, provisioner, args.role)
+  {}role: {}""".format(tag, addr, provisioner, fp_yaml, args.role)
 
 with open("{}/servers.yaml".format(list_location), 'a') as result_file:
     result_file.write(final_yaml)
 
 subprocess.Popen("startVM.sh {}".format(tag), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 print("Server {} has been requested and is booting. IP: {}".format(tag, addr))
+if fp:
+    print("Port forwarding enabled. Guest port {} -> host port {}".format(fp_map[args.role], hport))
