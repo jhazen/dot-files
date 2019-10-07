507c92b1bd77511c93db868afea80e132fb0d5c7
Jesse Hazen <jhazen532@gmail.com>
2019-10-07 14:22:06 -0700

    Adding shot.sh

---
+++ shot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z ${SHOTS_DIR+x} ]; then
+    SHOTS_DIR=/mnt/hgfs/ws/vimwiki/screenshots
+fi
+
+cd $SHOTS_DIR &> /dev/null
+F=`date +'%F_%k%M%S_%N'`
+import $F.png
+echo "screenshots/$F.png" | xclip
+cd - &> /dev/null
