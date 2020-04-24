7aa4cd816aaf7f07076caf95cd426885959f8eb7
Jesse Hazen <jhazen532@gmail.com>
2020-04-24 14:24:21 -0400

    fixing shot.sh

--- shot.sh
+++ shot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -z ${SHOTS_DIR+x} ]; then
-    SHOTS_DIR=/mnt/hgfs/ws/vimwiki/screenshots
+    SHOTS_DIR=~/vimwiki/screenshots
 fi
 
 cd $SHOTS_DIR &> /dev/null
 F=`date +'%F_%k%M%S_%N'`
 import $F.png
-echo "/screenshots/$F.png" | xclip
+echo "[[File:$SHOTS_DIR/$F.png]]" | xclip
 cd - &> /dev/null
