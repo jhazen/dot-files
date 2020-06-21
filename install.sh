606c8c301d1530589295166193c26ebe45f6583a
Jesse Hazen <jhazen532@gmail.com>
2020-06-21 15:39:31 -0700

    i3 configs

--- install.sh
+++ install.sh
@@ -30,14 +30,35 @@ fi
 
 function i3setup() {
   echo "i3"
-  if [ -f ~/.i3/config ]; then
-    mv ~/.i3/config $BACKUP_DIR/i3-$(date +%s)
+  if [ -f ~/.config/i3/config ]; then
+    mv ~/.config/i3/config $BACKUP_DIR/i3-$(date +%s)
   fi
-  if [ -L ~/.i3/config ]; then
-    rm ~/.i3/config
+  if [ -L ~/.config/i3/config ]; then
+    rm ~/.config/i3/config
   fi
-  if [ ! -d ~/.i3 ]; then
-    mkdir ~/.i3
+  if [ -f ~/.config/i3/conkyrc ]; then
+    mv ~/.config/i3/conkyrc $BACKUP_DIR/i3-$(date +%s)
+  fi
+  if [ -L ~/.config/i3/conkyrc ]; then
+    rm ~/.config/i3/conkyrc
+  fi
+  if [ -f ~/.config/i3/conkyrc2 ]; then
+    mv ~/.config/i3/conkyrc2 $BACKUP_DIR/i3-$(date +%s)
+  fi
+  if [ -L ~/.config/i3/conkyrc2 ]; then
+    rm ~/.config/i3/conkyrc2
+  fi
+  if [ ! -d ~/.config/terminator ]; then
+    mkdir ~/.config/terminator
+  fi
+  if [ ! -d ~/.config/i3 ]; then
+    mkdir ~/.config/i3
+  fi
+  if [ -f ~/.config/terminator/config ]; then
+    mv ~/.config/terminator/config $BACKUP_DIR/terminator-$(date +%s)
+  fi
+  if [ -L ~/.config/terminator/config ]; then
+    rm ~/.config/terminator/config
   fi
   if [ ! -d ~/bin ]; then
       mkdir ~/bin
@@ -45,14 +66,11 @@ function i3setup() {
   if [ -f ~/bin/wallpaper.sh ]; then
     rm ~/bin/wallpaper.sh
   fi
-  ln -s $DOTFILES/i3/config ~/.i3/config
+  ln -s $DOTFILES/i3/config ~/.config/i3/config
+  ln -s $DOTFILES/i3/conkyrc ~/.config/i3/conkyrc
+  ln -s $DOTFILES/i3/conkyrc2 ~/.config/i3/conkyrc2
+  ln -s $DOTFILES/terminator/config ~/.config/terminator/config
   ln -s $DOTFILES/i3/wallpaper.sh ~/bin/wallpaper.sh
-  ln -s $DOTFILES/i3/nordstart ~/bin/nordstart
-  ln -s $DOTFILES/i3/nordstop ~/bin/nordstop
-  if [ -f /usr/share/conky/conky_green ]; then
-    sudo rm /usr/share/conky/conky_green
-    sudo ln -s $DOTFILES/i3/conky_green /usr/share/conky/conky_green
-  fi
 }
 
 function vimsetup() {
