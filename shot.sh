e583b318cc7f22cf6f06569c6b7735fad1762f1e
Jesse Hazen <jhazen532@gmail.com>
2021-02-07 22:05:17 -0500

    some updates, cleaned up files

--- shot.sh
+++ shot.sh
@@ -5,10 +5,7 @@
 F=`date +'%F_%H%M%S_%N'`
 import ~/vimwiki/screenshots/$F.png
 
-#get shotval from $SHOTMAP
-SHOTVAL=`cat $SHOTMAP`
 # wiki format
 #echo "[[File:$SHOTS_DIR/$F.png]]" | xclip
 # markdown format
-echo -ne "![](./vimwiki/screenshots/$F.png)\n*Figure $SHOTVAL - *" | xclip
-shotmap_iterate
+echo -ne "![](./vimwiki/screenshots/$F.png)\n*Figure  - *" | xclip
