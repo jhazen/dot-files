03b50384755348a3990bc83c7d7183810b56d7cf
Jesse Hazen <jhazen532@gmail.com>
2021-10-31 22:45:16 -0400

    fix shot.sh

--- shot.sh
+++ shot.sh
@@ -8,4 +8,4 @@ import ~/vimwiki/screenshots/$F.png
 # wiki format
 #echo "[[File:$SHOTS_DIR/$F.png]]" | xclip
 # markdown format
-echo -ne "![](./vimwiki/screenshots/$F.png)\n*Figure  - *" | xclip
+echo -ne "![](./vimwiki/screenshots/$F.png)\n*Figure  - *" | xclip -selection clipboard
